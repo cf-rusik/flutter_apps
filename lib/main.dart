18f07f425be80470ceb1b863d7cce1b073ad866d
gammadov <gammadov@gmail.com>
2022-01-21 21:02:26 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const Text(
+      'Салам, интенсовцы!',
+      textDirection: TextDirection.ltr,
+      textAlign: TextAlign.center,
+    ),
+  );
+}
