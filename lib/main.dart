ccd858245b23b57d1192d78911c45031fdf97a9c
Sha.gam <gammadov@gmail.com>
2022-01-28 17:26:18 +0300

    lesson 41

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  // runApp(
+  //   const Align(
+  //     alignment: Alignment(-0.5, -0.9),
+  //     child: Text('Салам алейкум',
+  //         textDirection: TextDirection.ltr,
+  //         style: TextStyle(
+  //             color: Colors.tealAccent, // зеленый цвет текста
+  //             fontSize: 26, // высота шрифта 26
+  //             backgroundColor: Colors.grey) // черный цвет фона текста
+  //         ),
+  //   ),
+  // );
   runApp(
-    const Text(
-      'Салам, интенсовцы!',
-      textDirection: TextDirection.ltr,
-      textAlign: TextAlign.center,
+    const Padding(
+      padding: EdgeInsets.only(left: 100),
+      child: Center(
+        child: Text('Салам алейкум',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+                color: Colors.tealAccent, // зеленый цвет текста
+                fontSize: 26, // высота шрифта 26
+                backgroundColor: Colors.grey) // черный цвет фона текста
+        ),
+      ),
     ),
   );
 }
