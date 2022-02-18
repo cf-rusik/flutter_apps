b5cfd4b9d281c70d1bf6eadfca722acab5e383a3
cf-rusik <musrr@mail.ru>
2022-02-18 20:52:13 +0300

    180222

---
+++ lib/noble_gases/noble_gases_screen.dart
@@ -0,0 +1,17 @@
+/*---------------------------------------------------------
+
+ЗАДАНИЕ 3. Создание экрана.
+
+3.1. Создайте в этом файле stless виджет NobleGasesScreen.
+
+3.2.1. По умолчанию, метод Widget build(BuildContext context)
+       возвращает контейнер: return Container().
+       Замените контейнер на Scaffold().
+       В агрументе appBar укажите AppBar(title: ).
+
+3.2.2. Внутри Scaffold:
+       appBar: AppBar(title: ). В title поместите текст 'Благородные газы'.
+       backgroundColor: Colors.blueGrey
+       body: Внутри body поместите NobleGasRibbon(). Так, чтобы дизайн совпадал с картинкой из notion.
+
+---------------------------------------------------------*/
